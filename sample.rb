204b074b7405d3ee9ee64a205b4712de7d748f9e
Edward McCarthy <efmccarthy26@gmail.com>
2015-03-05 14:07:25 -0700

    relevant code changes. fixes all issues

--- sample.rb
+++ sample.rb
@@ -2,4 +2,13 @@ class Sample
   def self.do_stuff(objects)
     objects.map(&:clean)
   end
+
+  def break_stuff(sally)
+    sally.map do |puppies|
+      if puppies.git_confused
+        so_confused
+      else
+        so_not_impressed_with_code
+      end
+  end
 end
